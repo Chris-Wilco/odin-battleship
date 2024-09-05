dc57693efcc8e603c1d076c015f336eaa968045c
Chris-Wilco <christopher.wilcoski@gmail.com>
2024-09-04 17:29:19 -0700

    add UI elements and controls

--- src/modules/UserInterface.js
+++ src/modules/UserInterface.js
@@ -2,15 +2,84 @@ export default class UserInterface {
     constructor() {
         //create basic webpage layout with graphic representation of both player's game boards
 
-        this.generateInfoContainerHeader();
-        this.generateGameContainerVisual();
+        this.players = [];
+
+        this.pageBody = document.querySelector("body");
+        this.controlContainer = this.generateControlContainer();
+        this.populateControlContainer();
+        this.infoContainer = this.generateInfoContainer();
+        this.gameContainer = this.generateGameContainer();
+
+        /*         const addNewProjectButton = GenerateElement.generatePageElement(
+            "div",
+            ["nav-new-project-button", "button"],
+            navControlContainer,
+            "add project"
+        );
+        addNewProjectButton.addEventListener("click", () => {
+            this.createNewProject();
+            this.reloadNavContent();
+        }); */
     }
 
-    //create element that holds player stats and info above game play area
-    generateInfoContainerHeader() {}
+    generateControlContainer() {
+        return this.generatePageElement(
+            "div",
+            ["control-container"],
+            document.querySelector("body")
+        );
+    }
+
+    populateControlContainer() {
+        //new game button
+        //new player button
+        //end game button
+        //create players button
+    }
+
+    generateInfoContainer() {
+        return this.generatePageElement(
+            "div",
+            ["info-container"],
+            document.querySelector("body")
+        );
+    }
+
+    populateInfoContainer() {
+        //player1 info element
+        //player2 info element
+    }
+
+    reloadInfoContainer() {
+        this.clearInfoContainerContent();
+        this.populateInfoContainer();
+    }
+
+    clearInfoContainerContent() {
+        this.infoContainer.replaceChildren();
+    }
 
     //creates the element that will display both players game boards, as well as the new game, create player
-    generateGameContainerVisual() {}
+    generateGameContainer() {
+        return this.generatePageElement(
+            "div",
+            ["game-container"],
+            document.querySelector("body")
+        );
+    }
+
+    reloadGameContainerContent() {
+        this.clearGameContainerContent();
+        this.fillGameContainerContent();
+    }
+
+    fillGameContainerContent() {
+        //if no game yet, fill blank placeholder grid with no info?
+    }
+
+    clearGameContainerContent() {
+        this.gameContainer.replaceChildren();
+    }
 
     createGamePlayers() {
         //asks for user input to create players 1 and 2
@@ -50,4 +119,32 @@ export default class UserInterface {
 
     //add white hit dot to a single game square
     displayMiss(coords) {}
+}
+
+export function generatePageElement(
+    elementType,
+    elementClasses = {},
+    parentElement = null,
+    elementText = null
+) {
+    if (elementType == null) {
+        return;
+    }
+    const newElement = document.createElement(elementType);
+
+    if (elementClasses.length > 0) {
+        elementClasses.forEach((className) => {
+            newElement.classList.add(className);
+        });
+    }
+
+    if (elementText != null) {
+        newElement.textContent = elementText;
+    }
+
+    if (parentElement != null) {
+        parentElement.appendChild(newElement);
+    }
+
+    return newElement;
 }
